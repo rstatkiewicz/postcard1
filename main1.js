97dea113d2914953af5c3f94fa14300cb94c05f5
Radoslaw Statkiewicz <radoslawstatkiewicz2@gmail.com>
2020-01-06 00:52:57 -0500

    feat:js welcome

---
+++ main1.js
@@ -0,0 +1,12 @@
+
+
+const welcome = (name, age) => {
+    console.log('welcome ' + name + ', you are ' + age + ' years old.');
+    }
+
+   //welcome('Radek', 32);
+
+   const name = 'Radek';
+   const age = 32;
+
+   welcome(name, age); 
